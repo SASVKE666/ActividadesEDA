7a8bd8742744170204f471ea4310a21f206386d7
SASVKE666 <gerarlee@hotmail.com>
2024-07-16 22:34:30 -0700

    Update: hash en TablasHash

--- TablasHash/script.js
+++ TablasHash/script.js
@@ -1,43 +1,49 @@
-class HashTable{
-    constructor (){
-        this.size = 500; 
-        this.table = new Array(this.size);
-    }
-
-    put(key, value) {
-        const index = key % this.size;
-
-        if(this.table[index] === undefined){
-            this.table[index] = [];
-        }
-        
-        const entry = {"key" : key, "value" :value};
-        this.table[index].push(entry);
-    }
-
-    get(key) {
-        const index = key % this.size;
-        
-        if(this.table[index] === undefined){
-            return undefined;
-        }
-
-        const bucket = this.table[index];
-
-        let values = []
-        for(const entry of bucket){
-            if(entry.key === key){
-                return entry.value;
-            }
-        }
-        return values.length ? values : null;
-    }
+class HashTable {
+  constructor() {
+    this.size = 500;
+    this.table = new Array(this.size);
+  }
 }
 
-const tablaHash = new HashTable;
+HashTable.prototype.hash = function(key){
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashValue = (hashValue * 31 + key.charCodeAt(i)) % this.size;
+    }
+    return hashValue;
+};
 
-tablaHash.put(0, "Jose")
+HashTable.prototype.put = function (key, value) {
+  const index = this.hash(key);
+
+  if (this.table[index] === undefined) {
+    this.table[index] = [];
+  }
+
+  const entry = { key: key, value: value };
+  this.table[index].push(entry);
+};
+
+HashTable.prototype.get = function (key) {
+  const index = this.hash(key);
+  if (this.table[index] == undefined) {
+    return null;
+  }
+  const bucket = this.table[index];
+
+  let values = [];
+  for (const buck of bucket) {
+    if (buck.key == key) {
+      values.push(buck.value);
+    }
+  }
+  return values.length ? values : null;
+};
+
+const tablaHash = new HashTable();
+
+tablaHash.put(0, "Jose");
 tablaHash.put(6, "Diosito");
-tablaHash.put(6, "Jesus")
+tablaHash.put(6, "Jesus");
 console.log(tablaHash);
 console.log(tablaHash.get(6));
